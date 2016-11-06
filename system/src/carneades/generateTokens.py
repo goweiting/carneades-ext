53b1d89eca1fdb6c5e10a0323f486432924bc252
goweiting <gwt.543@gmail.com>
2016-11-06 11:39:22 +0000

    longest matching rule

--- system/src/carneades/generateTokens.py
+++ system/src/carneades/generateTokens.py
@@ -1,92 +1,171 @@
 import string
 
-_special_tokens = {
-    #  grouped in order or frequency
-    'SPACE': ' ',
-    'SEQUENCE_ENTRY': '-',
-    'MAPPING_VALUE': ':',
-    'LITERAL_BLOCK': '|',
-    'COMMENT': '#',
-    'POLARITY_SIGN': '~'
-}
-
-_ALPHA_DIGITS = string.ascii_letters + string.digits  # other characters allowed
-_special_list = _special_tokens.values()  # a list of values, for easy search
-
-pointer = -1  # the token we are currently lexing
-lineIdx = -1
-colIdx = -1
 
 class tokenizer(object):
     """
     Takes in a stream of character and tokenize them according to the rule
+    ---
+    DOCTEST:
+    Every stream is just a list of one str for modularised testing
+    >>> stream = ['# IGNORE MY COMMENT ! ##\\n', \
+                    '~\\n',\
+                     ':\\n']
+    >>> t = tokenizer(stream, 2)
+    >>> t.tokenize();
+    >>> t.tokens
+    [POLARITY_SIGN, MAPPING_VALUE]
+    ---
+
+    Correctly find word boundary in a sentence:
+    >>> stream2 = ['this is a sentence with 7 tokens\\n',\
+                    'only 4 tokens here #RIGHT\\n']
+    >>> t2 = tokenizer(stream2, 2);
+    >>> t2.tokenize();
+    >>> t2.tokens
+    [STMT, STMT, STMT, STMT, STMT, STMT, STMT, STMT, STMT, STMT, STMT]
     """
+
+
+    special_tokens = {
+        #  grouped in order or frequency
+        'SPACE': ' ',
+        'SEQUENCE_ENTRY': '-',
+        'MAPPING_VALUE': ':',
+        'LITERAL_BLOCK': '|',
+        'COMMENT': '#',
+        'POLARITY_SIGN': '~'
+    }
+
+    ALPHA_DIGITS = string.ascii_letters + string.digits  # other characters allowed
+    special_list = special_tokens.values()  # a list of values, for easy search
+
+    # Class variables
+    pointer = -1  # the index we are currently lexing
+    lineIdx = -1
+    colIdx  = -1
+    totalcount = -1;
+    tokens = [];
 
     def __init__(self, stream, indent_size):
         self.stream = stream
         self.indent_size = indent_size
         self.indent_stack = []
-        self.totalcount = sum([len(stream[i]) for i in range(0,len(stream))]);
-        self.tokens = []; # tokens from the stream
+        tokenizer.totalcount = sum([len(stream[i]) for i in range(0,len(stream))]); # update the total length
+
+        # reset the value everytime it is called
+        tokenizer.pointer = -1;
+        tokenizer.lineIdx = -1;
+        tokenizer.colIdx = -1;
+        tokenizer.tokens = [];
 
     def tokenize(self):
         """
         Iterate through all the characters in the file and find the boundaries
+        ----
+        Some testing:
+        ----
+        .find('#') always returns the smallest #
+        >>> line = ['# comment # comment\\n'];
+        >>> line[0].find('#')
+        0
+
         """
-        global pointer, lineIdx, colIdx
+        pointer = tokenizer.pointer + 1;
+        lineIdx = tokenizer.lineIdx + 1;
+        colIdx = tokenizer.colIdx + 1;
+        totalcount = tokenizer.totalcount
+        tokens = tokenizer.tokens
+        stream = self.stream
 
-        while pointer < self.totalcount: # iterate through all the character until the end of the source
-            pointer += 1;
-            colIdx += 1;
-            lineBoundary = len(self.stream[lineIdx])
+        while pointer < totalcount: # iterate through all the character until the end of the source
+            line = stream[lineIdx];
+            print(line)
+            lineBoundary = len(line) # this is local to the function, -1 because the line must end with a new line
 
-            line = lines[lineIdx];
-            if line.find('#'): # check if there's a comment in the line
-                self.lineBoundary =  line.find('#')
+            # ---------------------
+            #   COMMENT CHECKER:
+            # ---------------------
+            if line.find('#') != -1: # check if there's a comment in the line
+                lineBoundary = line.find('#') # and set the lineBoundary if there is
 
-            while self.colIdx < self.lineBoundary: # iterate through the line
-                c = self.lines[self.lineIdx][self.colIdx] #
-                # ignore comments:
-                while c != '#': # a comment must end with a new line
+            print('current pointer at ', str(pointer)) # DEBUG
+            print('Lineboundary = {}\tlineIdx = {}'.format(lineBoundary, lineIdx)) # DEBUG
+
+            # Now, iterate through each column (starting from 0)
+            while colIdx < lineBoundary: # iterate through the line
+                c = line[colIdx]
+                print(c, colIdx, pointer); # DEBUG
+
+                # -----------------------
+                #   SINGLE VALUE TOKENS
+                # -----------------------
+                token = None
+                if c == '~':
+                    token = Token(c, lineIdx, colIdx, 'POLARITY_SIGN')
+                elif c == ':':
+                    token = Token(c, lineIdx, colIdx, 'MAPPING_VALUE')
+                elif c == '-':
+                    token = Token(c, lineIdx, colIdx, 'SEQUENCE_ENTRY')
+                # elif c == ' ':
+                #
+                elif c in tokenizer.ALPHA_DIGITS: # start longest matching rule here!
                     # find boundary using longest matching rule to tokenise!
-                    (token, self.colIdx) = longest_matching(c)
-
-                # if comment found, fast foward the pointer to end of line:
-
-    def lookahead(self, stepsize=1):
-        """
-        Return the next element from the currentIdx, as long as it is within the lineBoundary
-        """
-        next_colIdx = self.colIdx + stepsize;
-        if next_colIdx < self.lineBoundary: # check if the next character is overflowing
-            return self.lines[lineIdx][next_colIdx]
-        else:
-            raise NewLineError('NewLine Expected But Not Found at ...')
+                    (token, colIdx) = self.longest_matching(line, lineIdx, colIdx, lineBoundary)
 
 
-    def longest_matching(self, c):
+                tokenizer.tokens.append(token)
+                colIdx += 1; # increment
+                pointer += 1;
+
+            # conditions at end of the line:
+            colIdx = 0;
+            delta = len(line) - lineBoundary
+            pointer += delta
+            lineIdx += 1
+
+
+    def longest_matching(self, line, lineIdx, colIdx, lineBoundary):
         """
         use the longest matching rule to find the word boundary,
         returns :class: token with boundary index
+        ------
+        :param: c - the current character it is evaluating
+        :param: lineBoundary - the limit it should go to
+        :param: colIdx
+        :return: The token found and the colIdx that it ends at
+        :rtype: :class:`token`
+        :rtype: colIdx - the new colIdx that longest matching ended with
         """
-        if c in special_list:
-            if c == '~': # polarity switch
-                token = token(c, lineIdx, colIdx, 'POLARITY_SIGN');
 
-            # elif c == ' ': # SPACE
-            # elif c == '-': # SEQUENCE_ENTRY
-            # elif c == ':': # MAPPING_VALUE
+        if colIdx < lineBoundary:
+            # if there are still characters remain in the line:
+            # find spaces:
+            space_idx = line.find(' ');
+            endline_idx = line.find('\n')
+            if space_idx + 1:
+                c = line[colIdx:space_idx] # slice the word out
+                token = Token(c, lineIdx, colIdx, 'STMT')
+                next_colIdx = space_idx;
+                break;
+
+            elif endline_idx + 1: # end of line case
+                c = line[colIdx:endline_idx]
+                token = Token(c, lineIdx, colIdx, 'STMT')
+                next_colIdx = endline_idx + 1; # sice endline is 2 character long
+                break;
+
+        return (token, next_colIdx)
+
+    def next(self, line, colIdx, lineBoundary):
+        pass
 
 
-
-
-
-class token(object):
+class Token(object):
     """
     A tokenizer convert the character into tokens
     """
 
-    def __init__(self, c, lineIdx, colIdx):
+    def __init__(self, c, lineIdx, colIdx, tok_type):
         """
         initialised the character
 
@@ -96,25 +175,29 @@ class token(object):
         :param colIdx : the column number of the file
         :parm sourceID : if multiple files are given in the :argument, this corresponds to the nth file.
         """
-        self.c = c
-        self.lineIdx = lineIdx
-        self.colIdx = colIdx
-        self.type = None
-
-        if c in special_list:  # add the token type into the tokens
-            if c == '\n':
-                self.c = ''  # replace it to prevent it from writing newline
-                self.type = 'PADDING_NEWLINE'
-            else:
-                for tok_type, val in special_char.items():
-                    if val == c:
-                        self.type = tok_type
-        else:
-            self.type = 'CHARACTER_DIGITS'
+        self.c          = c
+        self.lineIdx    = lineIdx
+        self.colIdx     = colIdx
+        self.tok_type   = tok_type
+        print('Token at {}, {} = {}'.format(lineIdx, colIdx, c)) # DEBUG
 
     def output(self):
         return (str(self.lineIdx) + ' ' + str(self.colIdx) +
                 ' ' + self.type + ' ' + str(self.c) + '\n')
 
     def __str__(self):  # same as output
-        return self.output()
+        return (str(self.tok_type))
+
+    def __repr__(self):
+        return (str(self.tok_type))
+
+# -----------------------------------------------------------------------
+# MAIN
+# -----------------------------------------------------------------------
+
+if __name__ == '__main__':
+    # do doctest here!
+    import doctest
+    # import carneades
+    print('Starting doctest!')
+    doctest.testmod(optionflags=doctest.NORMALIZE_WHITESPACE)
