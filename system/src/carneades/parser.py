acca58f05c73b861c912e8a11f2a46160bfe2785
goweiting <gwt.543@gmail.com>
2016-11-07 16:03:14 +0000

    started of AST! HARD

--- system/src/carneades/parser.py
+++ system/src/carneades/parser.py
@@ -1,4 +1,4 @@
-from collections import  namedtuple
+from collections import  namedtuple, defaultdict
 from error import *
 
 class parser(object):
@@ -19,7 +19,7 @@ class parser(object):
     >>> p = parser(tokens)
     >>> p.parse()
 
-    If more than one HEADER found, throw error:
+    If more than one HEADER found, throw ParseError:
     >>> stream=['    PROPOSITION\\n','PROPOSITION\\n']
     >>> t = tokenizer(stream); t.tokenize();
     >>> p = parser(t.tokens)
@@ -59,7 +59,7 @@ class parser(object):
         found = [];
         headers = ['PROPOSITION', 'ARGUMENT',
                    'ASSUMPTION', 'PROOFSTANDARD', 'PARAMETER']
-        previous_idx = len(self.tokens) -1 # starting from the back
+        previous_idx = len(self.tokens) # starting from the back
         for idx, tok in reversed(list(enumerate(self.tokens))):
             # find tok_type = `STMT`, and check if it is one of the headers
             if tok.tok_type == 'STMT':
@@ -69,55 +69,95 @@ class parser(object):
                     # store the start and stop indices:
                     if tok.c == 'PROPOSITION':
                         self.indices['PROPOSITION'] = [idx, previous_idx]
+                        # toks = self.tokens[idx, previous_idx];
+                        # self.get_proposition(toks);
+
                     elif tok.c == 'ASSUMPTION':
                         self.indices['ASSUMPTION'] = [idx, previous_idx]
+                        # toks = self.tokens[idx, previous_idx];
+                        # self.get_assumption(toks);
+
                     elif tok.c == 'ARGUMENT':
                         self.indices['ARGUMENT'] = [idx, previous_idx]
+                        # toks = self.tokens[idx, previous_idx];
+                        # self.get_argument(toks);
+
                     elif tok.c == 'PROOFSTANDARD':
                         self.indices['PROOFSTANDARD'] = [idx, previous_idx]
+                        # toks = self.tokens[idx, previous_idx];
+                        # self.get_proofstandard(toks);
+
                     elif tok.c == 'PARAMETER':
                         self.indices['PARAMETER'] = [idx, previous_idx]
+                        # toks = self.tokens[idx, previous_idx];
+                        # self.get_parameter(toks);
 
-                    previous_idx = idx-1;
+                    previous_idx = idx;
                     found.append(tok.c)
 
                 elif tok.c in found:
                     # prevent multiple usage of headers.
-                    raise ParseError('More than one label of {} is found'.format(tok.c))
+                    raise ParseError('More than one header of {} is found. Check that you only have one of each headers: {}'.format(tok.c, headers))
 
-    def get_proposition(self):
+    def get_proposition(self, toks):
         """
         find all the tokens that is relevant to proposition
         """
-        
 
-    def get_argument(self):
+
+    def get_argument(self, toks):
         """
         find all the tokens relevant to argument
         """
 
-    def get_assumption(self):
+    def get_assumption(self, toks):
         """
         find all the tokens relevant to assumption
         """
 
-    def get_proofstandard(self):
+    def get_proofstandard(self, toks):
         """
         find all the tokens relevant to proofstandard
         """
 
-    def get_parameter(self):
+    def get_parameter(self, toks):
         """
         find all the tokens relevant to parameter
         """
+        parameters = tree() # to be passed to caes
+        for i, tok in enumerate(toks):
+            # go through the tokens:
 
+def make_tree(object, tokens):
+    STMT = list()
+    for tok in tokens:
+        if tok.tok_type == 'STMT':
+            STMT.append(tok);
+        elif tok.tok_type == 'MAPPING_VALUE':
+            value = STMT.
 
-class indent_stack(object):
+class Node(object):
     """
-    defines the stack to calculate indent level
+    A tree data structure
     """
+    def __init__(self, data):
+        self.data = data
+        self.children = []
 
+    def add_child(self, obj):
+        self.children.append(obj)
 
+# class indent_stack(object):
+#     """
+#     defines the stack to calculate indent level
+#     """
+#     def __init__ (self):
+#         self.list = list();
+#
+#     def get_depth(self):
+#
+#     def add(self):
+#         self.list.append(1);
 
 # ---------------------------------------------------------------------------
 # MAIN
